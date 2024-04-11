8bd2f41358db77d2baab5791b1908e22b4ce4788
Nashib Jr <nashibjr@gmail.com>
2024-04-11 05:27:23 -0700

    Added a PageLayout component

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -7,13 +7,15 @@ import Input from "@/components/Input";
 import Button from "@/components/Button";
 import { useFormik } from "formik";
 import { emailSchema } from "@/validation/loginSchema";
+import Link from "next/link";
+import PageLayout from "./PageLayout";
 
 const ResetEmail = () => {
-  const [email, setEmail] = React.useState<string>("");
   const router = useRouter();
 
   const handleSubmit = (values: { email: string }) => {
     console.log(values, ">>>>");
+    router.push("/forgot-password/create-new-password");
   };
 
   const formik = useFormik({
@@ -24,7 +26,7 @@ const ResetEmail = () => {
   });
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2">
+    <PageLayout>
       <div>
         <div
           className="cursor-pointer md:hidden mt-20"
@@ -41,70 +43,51 @@ const ResetEmail = () => {
             Delightful Beauty
           </div>
         </div>
-        <div className="p-5 sm:w-[450px] sm:mx-auto">
-          <h2 className="text-2xl font-bold opacity-80 sm:text-3xl">
-            Forgot Password?
-          </h2>
-          <p className="text-sm font-semibold opacity-60 sm:text-base">
-            Enter your email to reset your password
+        <div className="sm:w-[450px] md:w-full max-w-[550px] sm:mx-auto md:pl-10 md:pr-10">
+          <p className="font-semibold opacity-70 text-center mt-14 mb-20 hidden md:block">
+            Already a member?{" "}
+            <Link href="/" className="text-[#4081e9]">
+              Sign In
+            </Link>
           </p>
-        </div>
-        <form
-          className="p-5 -mt-5 sm:w-[450px] sm:mx-auto"
-          onSubmit={formik.handleSubmit}
-        >
-          <Input
-            name="email"
-            type="email"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            placeholder="Email"
-            error={
-              formik.touched && formik.errors?.email && formik.errors?.email
-            }
-          />
-          <div className="flex gap-3 mt-5">
-            <Button
-              type="submit"
-              className="p-[10px] bg-[#4081e9] text-white rounded-lg"
-            >
-              Sumbit
-            </Button>
-            <Button
-              type="submit"
-              className="p-[10px] bg-blue-100 hover:bg-[#4081e9] hover:text-white text-bg-[#4081e9] rounded-lg"
-              onClick={() => router.replace("/")}
-            >
-              Cancel
-            </Button>
+          <div className="p-5">
+            <h2 className="text-2xl font-semibold opacity-80 sm:text-3xl lg:text-4xl">
+              Forgot Password?
+            </h2>
+            <p className="text-sm font-semibold opacity-60 sm:text-base md:mt-3 md:mb-4 mb-2 mt-3">
+              Enter your email to reset your password
+            </p>
           </div>
-        </form>
-      </div>
-      <div className="hidden md:block bg-[#4081e9] min-h-screen p-10 lg:p-20">
-        <div className="cursor-pointer" onClick={() => router.push("/")}>
-          <Image
-            src="/assets/images/dlogo.png"
-            width={70}
-            height={40}
-            alt="logo"
-            className="mx-auto"
-          />
-          <div className="text-white text-center mt-5 text-3xl font-bold">
-            Delightful Beauty
-          </div>
+          <form className="p-5 -mt-5" onSubmit={formik.handleSubmit}>
+            <Input
+              name="email"
+              type="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              placeholder="Email"
+              error={
+                formik.touched && formik.errors?.email && formik.errors?.email
+              }
+            />
+            <div className="flex gap-3 mt-5">
+              <Button
+                type="submit"
+                className="p-[10px] bg-[#4081e9] text-white rounded-lg"
+              >
+                Sumbit
+              </Button>
+              <Button
+                type="submit"
+                className="p-[10px] bg-blue-100 hover:bg-[#4081e9] hover:text-white text-bg-[#4081e9] rounded-lg"
+                onClick={() => router.replace("/")}
+              >
+                Cancel
+              </Button>
+            </div>
+          </form>
         </div>
-        <Image
-          src="/assets/images/auth-screens.png"
-          width={450}
-          height={450}
-          alt=""
-          className="mx-auto my-20"
-        />
-        <p className="text-4xl text-white font-bold mt-8 text-center">
-          Fast, Efficient and Reliable
-        </p>
       </div>
-    </div>
+    </PageLayout>
   );
 };
 
